e11ae3a6d69a9ce8acc33a4027097f9d5dd41ad9
Fernanda Torres Garcia <86421742+fernandatg@users.noreply.github.com>
2021-07-22 17:26:45 -0500

    Add files via upload

--- src/components/paginas/Crear.js
+++ src/components/paginas/Crear.js
@@ -1,50 +1,121 @@
-import React from 'react';
+import React, { useState } from "react";
+import ConexionApi from "../services/ConexionAxios";
 
-class Crear extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+function Crear () {
+
+    const variablesInicio={
+        nombre:"",
+        autor:"",
+        editorial:"",
+        edicion:"",
+        enlace:""
+    };
+
+    const [values, setValues]=useState(variablesInicio)
+
+    const onChange=(e)=>{
+        const{name, value}=e.target;
+        setValues({...values,[name]:value});
     }
-    render() { 
-        return ( 
+
+    const guardarLibros=async()=>{
+        await ConexionApi.post("/libro/crearLibro",{
+            nombre: values.nombre,
+            autor: values.autor,
+            editorial: values.editorial,
+            edicion: values.edicion,
+            enlace: values.enlace
+        }).then((res)=>{
+            console.log(res);
+            //console.log(data);
+        });
+    };
+
+    const onClick= (e) =>{
+        e.preventDefault();
+        /*alert(
+            "Los datos son:" +
+              values.matricula +
+              " " +
+              values.nombre +
+              " " +
+              values.correo +
+              " " +
+              values.telefono
+          );*/
+        guardarLibros();
+        setValues(variablesInicio);
+
+    }
+
+         return ( 
             <div className="card">
                 <div className="card-header">
                     Ingrese los datos del libro
                 </div>
                 <div className="card-body">
-                <form>
+                <form onSubmit={onClick}>
                 <div className="form-group">
                           <label htmlFor="">Nombre del libro</label>
-                          <input type="text" name="" id="" className="form-control" placeholder="Ingrese el Nombre"/>
+                          <input type="text" 
+                          name="nombre"
+                          onChange={onChange} 
+                          value={values.nombre}
+                          className="form-control" 
+                          placeholder="Ingrese el Nombre"/>
                         </div>   
                  </form>
-                 <form>
+                 <form onSubmit={onClick}>
                 <div className="form-group">
                           <label htmlFor="">Nombre del autor</label>
-                          <input type="text" name="" id="" className="form-control" placeholder="Ingrese el Nombre"/>
+                          <input type="text" 
+                           name="autor" 
+                           onChange={onChange}
+                           value={values.autor} 
+                           className="form-control" 
+                           placeholder="Ingrese el Nombre"/>
                         </div>   
                  </form>
-                 <form>
+                 <form onSubmit={onClick}>
                 <div className="form-group">
                           <label htmlFor="">Nombre de la Editorial</label>
-                          <input type="text" name="" id="" className="form-control" placeholder="Ingrese el Nombre"/>
+                          <input type="text" 
+                          name="editorial" 
+                          onChange={onChange}
+                          value={values.editorial} 
+                          className="form-control" 
+                          placeholder="Ingrese el Nombre"/>
                         </div>   
                  </form>
-                 <form>
+                 <form onSubmit={onClick}>
                 <div className="form-group">
                           <label htmlFor="">Edicion</label>
-                          <input type="text" name="" id="" className="form-control" placeholder="Ingrese el Dato"/>
+                          <input type="text" 
+                          name="edicion"
+                          onChange={onChange} 
+                          value={values.edicion} 
+                          className="form-control" 
+                          placeholder="Ingrese el Dato"/>
+                        </div>   
+                 </form>
+                 <form onSubmit={onClick}>
+                <div className="form-group">
+                          <label htmlFor="">Enlace</label>
+                          <input type="text" 
+                          name="enlace"
+                          onChange={onChange} 
+                          value={values.enlace} 
+                          className="form-control" 
+                          placeholder="Ingrese el Dato"/>
                         </div>   
                  </form>
                  <br></br>
-                 <button type="submit" className="btn btn-primary">Agregar libro</button>
-                 <button type="button" className="btn btn-secondary">Borrar</button>
+                 <button type="submit" className="btn btn-primary" onClick={onClick}>Agregar libro</button>
                 </div>
                 <div className="card-footer text-muted">
                 </div>
             </div>
          );
     }
-}
  
 export default  Crear;
--- src/components/paginas/Libros.js
+++ src/components/paginas/Libros.js
@@ -1,132 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Api from "../services/ConexionAxios";
 
 function Libros () {
+
+   const variablesInicio={
+      nombre:"",
+      autor:"",
+      editorial:"",
+      edicion:"",
+      enlace:""
+  };
+
+   const [values, setValues]=useState(variablesInicio);
+   const [libro, setLibros]=useState([]); 
+
+   const guardarLibros=async()=>{
+      await Api.post("/libro/crearLibro",{
+          nombre: values.nombre,
+          autor: values.autor,
+          editorial: values.editorial,
+          edicion: values.edicion,
+          enlace: values.enlace
+      }).then((res)=>{
+          console.log(res);
+          //console.log(data);
+      });
+      ListarLibros();
+  };
+
+
+      const ListarLibros=async()=>{
+      const respuesta=await Api.get('/libro/listarLibros');
+      setLibros(respuesta.data);
+      console.log(respuesta.data);
+  }
+
+  const EliminarLibro=async(id)=>{
+   const eliminar=await Api.delete(`/libro/eliminarLibro/${id}`);
+   console.log(eliminar.data);
+   ListarLibros();
+};
+
+
+       useEffect(() => {
+       ListarLibros();
+          }, [])
+
     return (
     <div>
       <div class="container">
          <div class="row">
+         {libro.map((libr)=>(
+                   
+                   <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                   <div class="card">
+                   <div class="card-body">
+                  
+                     <h5 class="card-title">{libr.nombre}</h5>
+                     <p class="card-text">{libr.autor}</p>
+                     <ul class="list-group list-group-flush">
+                      <li class="list-group-item">{libr.editorial}</li>
+                      <li class="list-group-item">{libr.edicion}</li>
+                      <a href={libr.enlace} class="card-link">Link del libro</a>
+                     </ul>
+                     <button className = "btn btn-outline-danger" onClick={()=>EliminarLibro(libr._id)}>Eliminar</button>
+    
+                   </div>
+                   </div>
+                </div>
 
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Ingeniria de Software un Enfoque Practico</h5>
-                 <p class="card-text">Roger S. Pressman</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">McGraw Hill</li>
-                  <li class="list-group-item">Septima Edicion</li>
-                 </ul>
-                 <a href="http://cotana.informatica.edu.bo/downloads/ld-Ingenieria.de.software.enfoque.practico.7ed.Pressman.PDF" class="btn btn-primary text-center">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">ManufacturA, INGENIERÍA Y TecNOLOGÍA</h5>
-                 <p class="card-text">Serope Kalpakjian, Steven R. Schmid</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Pearson</li>
-                  <li class="list-group-item">Cuarta Edicion</li>
-                 </ul>
-                 <a href="https://www.academia.edu/44318116/ManufacturA_INGENIERÍA_Y_TecNOLOGÍA" class="btn btn-primary">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Mecanicas de Suelos</h5>
-                 <p class="card-text">Alfonso Rico Rodríguez</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Limusa</li>
-                  <li class="list-group-item">Tercera Edicion</li>
-                 </ul>
-                 <a href="https://www.academia.edu/34972391/Mecánica_de_suelos_Tomo_III_Eulalio_Juárez_Badillo_y_Alfonso_Rico_Rodríguez" class="btn btn-primary">Ver libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Diseño Estrctural</h5>
-                 <p class="card-text">Roberto Meli Piralla</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Limusa</li>
-                  <li class="list-group-item">Segunda Edicion</li>
-                </ul>
-                 <a href="https://books.google.com.mx/books/about/Diseño_estructural.html?id=I2TKS13BB-EC&printsec=frontcover&source=kp_read_button&redir_esc=y#v=onepage&q&f=false" class="btn btn-primary">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Métodos numéricos para ingenieros</h5>
-                 <p class="card-text"> Raymond P. Canale, Steven C. Chapra</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">MCGRAW-HILL</li>
-                  <li class="list-group-item">Quinta Edicion</li>
-                 </ul>
-                 <a href="http://artemisa.unicauca.edu.co/~cardila/Chapra.pdf" class="btn btn-primary">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Tecnologia</h5>
-                 <p class="card-text">Ruben Castaño G.</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Paraninfo</li>
-                  <li class="list-group-item">Volumen 1</li>
-                 </ul>
-                 <a href="https://books.google.com.mx/books/about/Problemas_de_tecnología_industrial_I.html?id=CaNQAgAAQBAJ&printsec=frontcover&source=kp_read_button&redir_esc=y#v=onepage&q&f=false" class="btn btn-primary">Ver LIbro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Análisis y diseño de sistemas</h5>
-                 <p class="card-text">Kenneth E. Kendall, Julie E. Kendall</p>
-                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Pearson</li>
-                  <li class="list-group-item">Tercera Edicion</li>
-                 </ul>
-                 <a href="https://books.google.com.mx/books/about/Análisis_y_diseño_de_sistemas.html?id=vTu8G8nvIH8C&printsec=frontcover&source=kp_read_button&redir_esc=y#v=onepage&q&f=false" class="btn btn-primary">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-               <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Señales y sistemas</h5>
-                 <p class="card-text"> Michael J. Roberts</p>
-               <ul class="list-group list-group-flush">
-                  <li class="list-group-item">McGraw Hill</li>
-                  <li class="list-group-item">Primera Edicion</li>
-               </ul>
-               <div className="card-body">
-                 <a href="https://pdfcoffee.com/seales-y-sistemas-m-j-robertspdf-4-pdf-free.html" class="btn btn-primary">Ver Libro</a>
-                 <a href="#" class="btn btn-danger text-center">Eliminar</a>
-               </div>
-               </div>
-               </div>
-            </div>
+                  ))}
+           
 
          </div>
        </div>
@@ -135,3 +79,8 @@ function Libros () {
 }
 
 export default Libros
+
+
+
+
+
